9fea8703d2becb449bdc304ca1d0eac00c0cfaed
hantv15 <hantvph10714@fpt.edu.vn>
2021-12-14 10:15:34 +0700

    commit first

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,13 @@ use Monolog\Processor\HostnameProcessor;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+    |
 */
-
-Route::get('/', function () {
-    return view('admin.layouts.main');
-});
+Route::get('/', [HostpitalController::class, 'view'])->name('view.hospital');
+Route::get('/hostpital/add', function(){
+    return view('admin.hostpital.add');
+})->name('add.hostpital');
+Route::post('/hostpital/add', [HostpitalController::class, 'saveAdd']);
+// Route::get('/hostpital/edit/{id}', [HostpitalController::class, 'formEdit'])->name('edit.hostpital');
+// Route::post('/hostpital/edit/{id}', [HostpitalController::class, 'saveEdit']);
+// Route::post('/hostpital/remove', [HostpitalController::class, 'remove']);
